f768fa14e7fe08fe9fd80bd2eecfd541ff6a8f74
pradeepkumar673 <pradeepcastleofficial@gmail.comexit>
2025-11-27 13:17:34 +0530

    2nd

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -80,4 +80,5 @@ const taskSchema = new mongoose.Schema({
   timestamps: true
 });
 
+
 module.exports = mongoose.model('Task', taskSchema);
---
+++ backend/server.js
@@ -0,0 +1,22 @@
+﻿const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Backend is working!' });
+});
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/taskbooking')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('MongoDB error:', err.message));
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log('✅ Backend server running on http://localhost:5000');
+});
